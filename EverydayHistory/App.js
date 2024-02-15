b1492117a5b7b597e0a0ace7eccd9d5f6d74463a
GoodGuyCuban <marcos.perezperera@gmail.com>
2024-02-15 14:14:08 -0700

    Added image support

--- EverydayHistory/App.js
+++ EverydayHistory/App.js
@@ -8,6 +8,7 @@ import {
   View,
   Linking,
   SafeAreaView,
+  Image,
 } from "react-native";
 import API from "./API";
 
@@ -66,6 +67,7 @@ export default function App() {
     const words = text.split(" ");
     let i = 0;
     const elements = [];
+
     while (i < words.length) {
       let found = false;
       for (let j = Math.min(i + 4, words.length); j > i; j--) {
@@ -88,6 +90,9 @@ export default function App() {
           );
           i = j;
           found = true;
+          if (page.thumbnail) {
+            lastPageWithThumbnail = page;
+          }
           break;
         }
       }
@@ -96,34 +101,68 @@ export default function App() {
         i++;
       }
     }
+
     return <Text>{elements}</Text>;
+  };
+
+  const renderImage = (pages, text) => {
+    let pageWithThumbnail;
+    if (text.match(/\bwar(s)?\b[:]?/gi)) {
+      pageWithThumbnail = pages.reverse().find((page) => page.thumbnail);
+    } else {
+      pageWithThumbnail = pages.find((page) => page.thumbnail);
+    }
+
+    if (pageWithThumbnail) {
+      return (
+        <Image
+          key={pageWithThumbnail.pageid}
+          source={{ uri: pageWithThumbnail.thumbnail.source }}
+          style={styles.Image}
+          resizeMode="contain"
+        />
+      );
+    }
+    return null;
+  };
+
+  const getDayWithSuffix = (day) => {
+    if (day % 10 === 1 && day !== 11) {
+      return `${day}st`;
+    } else if (day % 10 === 2 && day !== 12) {
+      return `${day}nd`;
+    } else if (day % 10 === 3 && day !== 13) {
+      return `${day}rd`;
+    } else {
+      return `${day}th`;
+    }
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.container}>
+      <View style={styles.topBar}>
         <Text style={styles.titleText}>Everyday History</Text>
-
-        <View style={styles.container}>
-          <Text style={styles.headingText}>
-            On {day} of {monthWord} these events happened:
-          </Text>
-
-          <FlatList
-            contentContainerStyle={{
-              padding: 20,
-            }}
-            data={eventsList}
-            renderItem={({ item }) => (
-              <View style={styles.item}>
-                <Text style={styles.itemHeader}>{item.year}:</Text>
-                {renderTextWithLinks(item.text, item.pages)}
-              </View>
-            )}
-          />
-        </View>
-        <StatusBar style="auto" />
+        <Text style={styles.headingText}>
+          On the {getDayWithSuffix(day)} of {monthWord},
+        </Text>
       </View>
+      <View style={styles.container}>
+        <FlatList
+          bounces={false}
+          contentContainerStyle={{
+            padding: 20,
+          }}
+          data={eventsList}
+          renderItem={({ item }) => (
+            <View style={styles.item}>
+              <Text style={styles.itemHeader}>{item.year}:</Text>
+              {renderTextWithLinks(item.text, item.pages)}
+              {renderImage(item.pages, item.text)}
+            </View>
+          )}
+        />
+      </View>
+      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
@@ -132,16 +171,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
+    alignItems: "stretch",
     justifyContent: "center",
+    width: "100%",
+  },
+  topBar: {
+    backgroundColor: "white",
+    alignItems: "center",
+    width: "100%",
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
   },
   titleText: {
     fontSize: 20,
     fontWeight: "bold",
+    margin: 5,
   },
   headingText: {
     fontSize: 16,
     fontWeight: "bold",
+    margin: 5,
   },
   item: {
     padding: 10,
@@ -157,5 +206,11 @@ const styles = StyleSheet.create({
   },
   linkText: {
     color: "blue",
+  },
+  Image: {
+    width: 300,
+    height: 200,
+    alignSelf: "center",
+    margin: 10,
   },
 });
